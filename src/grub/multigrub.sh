a20d911935947c90ae8db1ad62c4d9abab4440c7
mk-pmb <mk-pmb@users.noreply.github.com>
2020-03-07 16:18:45 +0100

    multigrub: accept non-ESP boot dirs

--- src/grub/multigrub.sh
+++ src/grub/multigrub.sh
@@ -3,68 +3,96 @@
 
 
 function multigrub_main () {
-  local ESP_MPT="${1:-/target/boot}"; shift
-  ESP_MPT="${ESP_MPT%/}"
-  local EFI_DIR="$ESP_MPT/EFI"
-
   local SUDO_CMD=
   [ "$USER" == root ] || SUDO_CMD='sudo -E'
 
-  multigrub_ensure_efi_dir || return $?
+  local BOOT_DIR="$1"
+  if [ -z "$BOOT_DIR" ]; then
+    for BOOT_DIR in /target/{mnt/esp,boot}; do
+      [ -d "$BOOT_DIR" ] && break
+    done
+    echo "D: No boot directory given. Defaulting to '$BOOT_DIR'." >&2
+  fi
+  multigrub_is_plausible_boot_dir || return $?
 
-  mountpoint -q -- "$ESP_MPT" || return 4$(
-    echo "E: ESP path '$ESP_MPT' seems to not be a mounted mountpoint." >&2)
-  local ESP_PTN="$(mount | grep -Fe " on $ESP_MPT type " | grep -Poe '^/\S+')"
-  [ -b "$ESP_PTN" ] || return 4$(echo "E: cannot find ESP by mountpoint" >&2)
+  local EFI_DIR="$BOOT_DIR/EFI"
+  local ESP_MPT=
+  multigrub_verify_esp_mpt || return $?
+
+  local ESP_PTN="$(findmnt --noheadings --output source --evaluate \
+    --target "$ESP_MPT")"
+  [ -b "$ESP_PTN" ] || return 4$(echo "E: cannot find ESP by mountpoint" \
+    "'$ESP_MPT'" >&2)
   local ESP_DISK='
     s~^(/dev/[sh]d[a-z]+)[0-9]+$~\1~p
     '
   ESP_DISK="$(<<<"$ESP_PTN" sed -nrf <(echo "$ESP_DISK"))"
   [ -b "$ESP_DISK" ] || return 4$(
     echo "E: cannot find disk for ESP '$ESP_PTN' mounted on '$ESP_MPT'" >&2)
+
   # :TODO: verify it's a proper ESP
 
   local GI_OPTS=(
-    --{efi,boot}-directory="$ESP_MPT"
+    --boot-directory="$BOOT_DIR"
+    --efi-directory="$ESP_MPT"
     --no-nvram
     --skip-fs-probe
     --removable
     -- "$ESP_DISK"
     )
+  local PLATFS=(
+    # Fallbacks first, best platforms last, so their changes will prevail.
+    pc
+    efi
+    )
   local PLATF=
-  for PLATF in /usr/lib/grub/*-{efi,pc}/; do
-    [ -f "$PLATF/modinfo.sh" ] || continue
-    PLATF="$(basename -- "${PLATF%/}")"
-    $SUDO_CMD grub-install --target="$PLATF" "${GI_OPTS[@]}" || return $?
+  local GRUB_CMD="grub-install --target="
+  echo "D: cmd template: ${GRUB_CMD}PLAT ${GI_OPTS[*]}"
+
+  for PLATF in "${PLATFS[@]}"; do
+    for PLATF in /usr/lib/grub/*-"$PLATF"/; do
+      [ -f "$PLATF/modinfo.sh" ] || continue
+      PLATF="$(basename -- "${PLATF%/}")"
+      $SUDO_CMD $GRUB_CMD"$PLATF" "${GI_OPTS[@]}" || return $?
+    done
   done
 }
 
 
-function multigrub_ensure_efi_dir () {
-  [ -d "$EFI_DIR" ] && return 0
-  multigrub_is_plausible_esp || return 3$(
-    echo "E: '$EFI_DIR' seems to not be a directory and '$ESP_MPT'" \
-      "doesn't looks like a plausible ESP." >&2)
-  $SUDO_CMD mkdir -- "$EFI_DIR" || return $?
-}
+function multigrub_is_plausible_boot_dir () {
+  local UNBOOT="E: Probably not bootable: Boot directory '$BOOT_DIR' $(
+    )contains none of the files we'd typically expect, especially not"
 
-
-function multigrub_is_plausible_esp () {
   local ITEM=
-
-  for ITEM in "$ESP_MPT"/memtest86*.{bin,elf}; do
-    [ -f "$ESP_MPT" ] && return 0   # any memtest is good enough
+  for ITEM in "$BOOT_DIR"/memtest86*.{bin,elf}; do
+    [ -f "$ITEM" ] && return 0   # any memtest is good enough
   done
 
-  for ITEM in "$ESP_MPT"/initrd.img-[0-9]* ''; do
+  local GR="$BOOT_DIR"/grub
+  [ -f "$GR"/grub.cfg ] && return 0 # Seems well-established.
+  [ -f "$GR"/sgd/main.cfg ] && return 0 # SuperGRUB Disk, a good choice.
+
+  for ITEM in "$BOOT_DIR"/initrd.img-[0-9]* ''; do
     [ -f "$ITEM" ] && break
-    [ -n "$ITEM" ] || return 3   # no initrd = not a typical Ubuntu
+    [ -n "$ITEM" ] || return 3$(echo "$UNBOOT an initrd." >&2)
   done
-  for ITEM in "$ESP_MPT"/vmlinuz-[0-9]* ''; do
+  for ITEM in "$BOOT_DIR"/vmlinuz-[0-9]* ''; do
     [ -f "$ITEM" ] && break
-    [ -n "$ITEM" ] || return 3   # no vmlinuz = not a typical Ubuntu
-    return 3
+    [ -n "$ITEM" ] || return 3$(echo "$UNBOOT a vmlinuz." >&2)
   done
+  # echo "D: found an initrd and a vmlinuz." >&2
+}
+
+
+function multigrub_verify_esp_mpt () {
+  $SUDO_CMD mkdir --parents "$EFI_DIR"
+  [ -d "$EFI_DIR" ] || return 4$(echo "E: not a directory: $EFI_DIR" >&2)
+  local ED_MPT="$(stat --dereference --format='%m' -- "$EFI_DIR")"
+  local ED_PAR="$(readlink -m -- "$EFI_DIR/..")"
+  [ "$ED_MPT" == "$ED_PAR" ] || return 4$(
+    echo "E: EFI directory's mountpoint '$ED_MPT'" \
+      "is not its parent directory '$ED_PAR'." >&2)
+  ESP_MPT="$ED_MPT"
 }
 
 
